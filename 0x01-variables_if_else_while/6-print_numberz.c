5fe068321dc7448a7da4d2a6beef53d41ce95e1d
assisi-francis <doncyco123@gmail.com>
2022-09-09 14:10:10 -0700

    add print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print single digits of base 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+	}
+	putchar(10);
+
+	return (0);
+}
