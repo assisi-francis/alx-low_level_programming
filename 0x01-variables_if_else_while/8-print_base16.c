f4cf5f1ca6ec6f3e9b36fc2b5a9286a293d9bc4d
assisi-francis <doncyco123@gmail.com>
2022-09-09 14:39:59 -0700

    add print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print hexadecimal numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 48)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else if (i > 41)
+		{
+			putchar(i - 10 + 'A');
+		}
+		i++
+	}
+	putchar(10);
+
+	return (0);
+}
