0296f884912f8d1aa87bcbcb31e11074f34babe6
assisi-francis <doncyco123@gmail.com>
2022-09-13 15:59:17 -0700

    add abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of integrer
+ *
+ * @n - n is an integer
+ *
+ * Return 1 positive -1 negative o is 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
