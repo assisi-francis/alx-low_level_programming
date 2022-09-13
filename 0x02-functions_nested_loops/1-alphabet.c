68c6a9b2800f388e96b20b71a134fb80c5b4cd84
assisi-francis <doncyco123@gmail.com>
2022-09-13 13:59:31 -0700

    add alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet: prints alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (aphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	-putchar('\n');
+}
