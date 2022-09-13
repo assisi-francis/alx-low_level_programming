b45b9552f461c91f5e8ce4d9e7ff2d767cbdadae
assisi-francis <doncyco123@gmail.com>
2022-09-13 14:11:42 -0700

    chage

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet: prints alphabet in lowercase
- *
- * Description: prints alphabets in small letters
+ * print_alphabet - Prints alphabet in lowercase
  *
  * Return: Always 0 (success)
  */
