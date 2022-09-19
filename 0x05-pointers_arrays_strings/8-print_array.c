23b0a2cef94da068b5023769ae187575ec76b6e7
assisi-francis <doncyco123@gmail.com>
2022-09-19 16:06:50 -0700

    add array print

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: pointer to array of integers
+ * @n: pointer to number of items to print
+ * return: void
+ */
+
+void print_array(int *a, int *n)
+{
+	int i, count;
+
+	i = 0;
+	count = 1;
+	if (n < 0)
+		n = 0;
+	if (n > 0)
+	{
+		while (a[i] != '\0' && count < n)
+		{
+			printf("%d, ", a[i]);
+			i++;
+			count++;
+		}
+		printf("%d\n", a[i]);
+	}
+}
