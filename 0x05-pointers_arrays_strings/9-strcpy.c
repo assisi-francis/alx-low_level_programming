f8f7e7acf5f0e760299bf14d1f9b2fc77bfcc2cf
assisi-francis <doncyco123@gmail.com>
2022-09-19 16:27:45 -0700

    add strcp

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to bt src into dest
+ * @dest: pointer destination
+ * @src: pointer to source
+ * Return: char with copy of string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	for (i = 0; *(src = i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
