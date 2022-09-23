7273fddf9114cdbec8e0d6a6adb944495670dae0
assisi-francis <doncyco123@gmail.com>
2022-09-23 07:37:04 -0700

    add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -23,9 +23,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
+
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+
 	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
@@ -45,8 +47,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
+
 		bg--, size_r--;
 	}
+
 	if (*(r) == '0')
 		return (r + 1);
 	else
