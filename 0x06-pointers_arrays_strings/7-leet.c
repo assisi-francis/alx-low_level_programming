65ebaa94adf617571dd98a64a35f4d862cc4412b
assisi-francis <doncyco123@gmail.com>
2022-09-23 03:15:46 -0700

    add again

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,20 +8,22 @@
 
 char *leet(char *s)
 {
-	int i;
+	int count = 0, i;
+	int lowLetters[] = {97, 101, 111, 116, 108};
+	int uppLetters[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + count) != '\0')
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + count) == lowLetters[i] || *(s + count) == upperLetters[i])
+			{
+				*(s + count) = num[i];
+				break;
+			}
+		}
+		count++;
+
 	return (s);
 }
