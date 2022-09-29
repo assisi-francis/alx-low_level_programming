8adfed7a537eedf84ebeac996f7bb11b986f433a
assisi-francis <doncyco123@gmail.com>
2022-09-29 15:29:01 -0700

    add

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_,atch(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
+/**
+ * strlen_no_wilds - Returns the length of a string
+ * @str: the string to be measured
+ * Return: int length
+ */
+int strlen_no_wilds(char *str)
+{
+	int len = 0, index = 0;
+
+	if (*(str + index))
+	{
+		if (*str != '*')
+			len++;
+
+		index++;
+		len += strlen_no_wilds(str + index);
+	}
+	return (len);
+}
+
+/**
+ * iterate_wild - iterates throught a string
+ * @wildstr: the string to be iterated
+ * Return: void
+ */
+
+void iterate_wild(char **wildstr)
+{
+	if (**wildstr == '*')
+	{
+		(wildstr)++;
+		iterate_wild(wildstr);
+	}
+}
+
+/**
+ * postfix_match - checks str matches
+ * @str: the string to be matched
+ * @postfix: the postfix
+ * return: reversed string
+ */
+char *postfix_match(char *str, char *postfix)
+{
+	int str_len = strlen_no_words(str) - 1;
+	int posfix_len = strlen_no words(spotive) - 1;
+
+	if (*postfix == '*')
+		iterate_wild(&positive);
+
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (potfix_match(str, postfix));
+	}
+	return (postfix);
+}
+
+/**wildcmp - Compare two strings
+ * @n1: the first string
+ * @s: the second string
+ * return: 1 if idental and u
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		iterate_wild(&s2);
+		s2 = posfix_match (s1, s2)
+	}
+	if (*s2 == '\0')
+		return (1);
+	if (*s1 != *s2)
+		return (0);
+	return (wildcmp(++s1, ++s2;
+}
+
