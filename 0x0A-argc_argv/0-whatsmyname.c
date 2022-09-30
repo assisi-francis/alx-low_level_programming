022b8e7b3b73c8779caa12276ed60144bfd7546a
assisi-francis <doncyco123@gmail.com>
2022-09-30 14:57:17 -0700

    whats my nae

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * maon - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+
+int main(__attribute__((unused)) int argc, char *agrv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
