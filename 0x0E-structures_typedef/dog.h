a688c32b14e5795bf335b57ce8ed8f1574702914
assisi-francis <doncyco123@gmail.com>
2022-10-10 12:58:24 -0700

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+int _putchar(char c);
+/**
+ * struct dog - best friend of humankind
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* !DOG_H */
