90993a24103fe8d55848b4ad94506e015d2573f5
assisi-francis <doncyco123@gmail.com>
2022-10-10 14:26:36 -0700

    add

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * _strdup - returns a pointer to a newly allocated space in memory
  * @str: string
  * Return: pointer to newly allocated space in memory
