622fc009d0b515c7cbe60cff5dbb7a9acc432f99
assisi-francis <doncyco123@gmail.com>
2022-12-15 13:02:12 -0800

    add

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://https://github.com/Nancyiddy/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+gcc *.o -fPIC -shared -o libruin.so
+LD_PRELOAD=$WPD/libruin.so
